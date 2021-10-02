28d0401905e1f8fbd21482ff2fc0faf3cdda92e6
Richard Diaz <34225851+rokiman33@users.noreply.github.com>
2021-10-02 17:35:04 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angularprd-escalab'));
+app.use(express.static(__dirname + '/dist/cliente'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angularprd-escalab/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cliente/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
